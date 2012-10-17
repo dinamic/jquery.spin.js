9c08ec0cd52143fc2e6424fbe022a5869f05b059
Nikola Petkanski <nikola@petkanski.com>
2012-10-17 11:21:13 +0000

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ jquery.spin.js
 ==============
 
 Integration layer between jquery.js and spin.js
+
+$('#someElement').spin();
+toggle displaying a spinner within a container element
+	
+show a spinner over a given element; creates absolutely possitioned div and renders the spinner inside. execute .spinUnblock() to hide it.
+	$('#someElement').spinBlock();
---
+++ jquery.spin.js
@@ -0,0 +1,71 @@
+(function($){
+    $.fn.spin = function(opts) {
+        this.each(function() {
+            var $this = $(this),
+                data = $this.data();
+
+            if (data.spinner) {
+                data.spinner.stop();
+                delete data.spinner;
+            }
+            if (opts !== false) {
+                data.spinner = new Spinner($.extend({color: $this.css('color')}, opts)).spin(this);
+            }
+        });
+        return this;
+    };
+    
+    $.fn.spinBlock = function(opts) {
+        this.each(function() {
+            var $this = $(this);
+            var container = $this;
+            var data = container.data();
+            
+            if (container.parents('.ui-dialog').length > 0) {
+                container = $(container.parents('.ui-dialog')[0]);
+                data = container.data();
+            }
+            
+            if (!data.spinBlocker) {
+                data.spinBlocker = $('<div class="spinblocker"></div>');
+                data.spinBlocker.css({
+                    position: 'absolute',
+                    zIndex: 9999,
+                    top: container.offset().top + 'px',
+                    left: container.offset().left + 'px',
+                    width: container.outerWidth() + 'px',
+                    height: container.outerHeight() + 'px'
+                });
+
+                data.spinBlocker.appendTo(container.parents('body'));
+            }
+
+            data.spinBlocker.show(opts);
+            data.spinBlocker.spin();
+        });
+        
+        return this;
+    };
+    
+    $.fn.spinUnblock = function(opts) {
+        this.each(function() {
+            var $this = $(this);
+            var container = $this;
+            var data = container.data();
+            
+            if (container.parents('.ui-dialog').length > 0) {
+                container = $(container.parents('.ui-dialog')[0]);
+                data = container.data();
+            }
+                
+            if (!data.spinBlocker) {
+                return this;
+            }
+            
+            data.spinBlocker.data().spinner.stop();
+            data.spinBlocker.hide();
+        });
+        
+        return this;
+    };
+})(jQuery);
